95197489314f82091d699f31bddc5d995d96945e
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-05-29 15:32:42 +0100

    Initial Commit

---
+++ Animation Technique Project/Animation_Technique_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_Technique_ProjectApp.swift
+//  Animation Technique Project
+//
+//  Created by Mehmet Alp Sönmez on 29/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Animation_Technique_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animation Technique Project/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Animation Technique Project
+//
+//  Created by Mehmet Alp Sönmez on 29/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let letters = Array("Hello SwiftUI")
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(colors: [.purple, .black], startPoint: .topLeading, endPoint: .bottomTrailing)
+            HStack(spacing: 0) {
+                ForEach(0..<letters.count, id: \.self) { num in
+                    Text(String(letters[num]))
+                        .padding(5)
+                        .font(.title)
+                        .background(enabled ? .blue : .red)
+                        .offset(dragAmount)
+                        .animation(.linear.delay(Double(num) / 20), value: dragAmount)
+                    
+                }
+            }
+            .gesture(
+                DragGesture()
+                    .onChanged { dragAmount = $0.translation }
+                    .onEnded { _ in
+                        dragAmount = .zero
+                        enabled.toggle()
+                    }
+            )
+            
+            
+        }.ignoresSafeArea()
+    }
+}
+
+#Preview {
+    ContentView()
+}
